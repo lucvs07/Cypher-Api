f88de23d9f28387f31630caf908a8744920401c5
Lucas Grecco <lucas.grecco@asaas.com.br>
2026-04-26 17:24:58 -0300

    Chore(Cypher) : Estrutura Inicial do Projeto Spring Boot

---
+++ src/test/java/com/dev4/cypher/CypherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev4.cypher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CypherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
